312b1ffacf2cbe336ed867df21a43db02f6ef56a
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-05-24 12:13:00 +0200

    added logging

--- src/biz/playr/StartPlayrServiceAtBootReceiver.java
+++ src/biz/playr/StartPlayrServiceAtBootReceiver.java
@@ -3,14 +3,17 @@ package biz.playr;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class StartPlayrServiceAtBootReceiver extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent serviceIntent = new Intent(context, biz.playr.PlayrService.class);
-            context.startService(serviceIntent);
-        }
-    }
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	Log.i("biz.playr.StartPlayrServiceAtBootReceiver", "overide onReceive");
+	if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+		Intent serviceIntent = new Intent(context, biz.playr.PlayrService.class);
+		context.startService(serviceIntent);
+		Log.i("biz.playr.StartPlayrServiceAtBootReceiver", "onReceive: started PlayrService");
+		}
+	}
 }
