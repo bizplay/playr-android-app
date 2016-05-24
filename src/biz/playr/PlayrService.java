750e8ce3c62788467fc91b2c7e1c7cde78c6550b
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-05-24 12:23:24 +0200

    added logging, TODO start main activity, although that seems unecessary at the moment becuase of the intent that is used for the main activity (launcher)

--- src/biz/playr/PlayrService.java
+++ src/biz/playr/PlayrService.java
@@ -3,13 +3,29 @@ package biz.playr;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class PlayrService extends Service{
 
 	@Override
-	public IBinder onBind(Intent arg0) {
+	public void onCreate() {
+		Log.i("biz.playr.PlayrService","override onCreate");
+		super.onCreate();
+	}
+	@Override
+	public int onStartCommand (Intent intent, int flags, int startId) {
+		Log.i("biz.playr.PlayrService","override onStartCommand");
+		return super.onStartCommand(intent, flags, startId);
+	}
+	@Override
+	public void onDestroy() {
+		Log.i("biz.playr.PlayrService","override onDestroy");
+		super.onDestroy();
+	}
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i("biz.playr.PlayrService","override onBind");
 		// TODO start MainActivity
 		return null;
 	}
-
 }
