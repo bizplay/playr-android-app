7aafec524430c33801f02561a0d5fafdda5b4edd
Bob Groeneveld <bob.groeneveld@gmail.com>
2016-11-01 13:05:38 +0100

    fixed readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Playr for Android
 =================
 
 ##Introduction
-This is an application to play digital signage content from the playr.biz provider.
+This is an application to play digital signage content.
 
-The application shows the content full screen on Android devices running Android 4.1 (Jelly Bean, june 2012) or later. 
+The application shows the content full screen on Android devices running Android 4.4 or later. 
--- src/biz/playr/MainActivity.java
+++ src/biz/playr/MainActivity.java
@@ -301,9 +301,9 @@ public class MainActivity extends Activity {
 			// a general rule, you should design your app to hide the status bar whenever you
 			// hide the navigation bar.
 			int uiOptions =  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-								| View.SYSTEM_UI_FLAG_FULLSCREEN
-								| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-								| View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+							| View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 			decorView.setSystemUiVisibility(uiOptions);
 		}
 		// Remember that you should never show the action bar if the
